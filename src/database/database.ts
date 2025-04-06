15c363d90c9595a439545721c7410cc43e2ca6fd
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-06 19:57:58 -0300

    feat: add envroinment variables

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as knexSetup, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE URL NOT FOUND')
+}
 
 export const config:Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db'
+    filename: process.env.DATABASE_URL
   },
   useNullAsDefault: true,
   migrations: {
