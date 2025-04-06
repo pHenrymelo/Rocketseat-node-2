fa7e925a0e77ab5179c2601bb6739c38defebe8f
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-06 19:20:35 -0300

    feat: add first migration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,8 +1,16 @@
-import { knex as knexSetup } from 'knex'
+import { knex as knexSetup, Knex } from 'knex'
 
-export const knex = knexSetup({
+export const config:Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db'
+    filename: './db/app.db'
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
   }
-})
+
+}
+
+export const knex = knexSetup(config)
