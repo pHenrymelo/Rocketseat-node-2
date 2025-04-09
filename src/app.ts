06b63494199d34f69553fa477eda7df4cd6e8dba
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-09 10:05:06 -0300

    feat: add prepare dev db preHook

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { transactionRoutes } from './routes/transactions'
+import { execSync } from 'node:child_process'
 
 export const app = fastify()
+
+app.addHook('onReady', async () => {
+  await execSync('pnpm knex migrate:latest')
+})
 
 app.register(cookie)
 app.register(transactionRoutes, {
